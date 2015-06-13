a07dfbbfda34057fd7a344777b8364de9b2a363b
Pujubuju <truetormentedsoul@gmail.com>
2015-06-13 15:29:18 +0200

    refactor main

--- TestSolution/Apps/DashboardApplication/DashboardApp.OwinSelfHost/Program.cs
+++ TestSolution/Apps/DashboardApplication/DashboardApp.OwinSelfHost/Program.cs
@@ -7,13 +7,38 @@ namespace DashboardApp.OwinSelfHost
     {
         static void Main()
         {
-            const string baseAddress = "http://localhost:9000/";
-            using (WebApp.Start<Startup>(baseAddress))
+            var options = CreateStartupOptions();
+            using (WebApp.Start<Startup>(options))
             {
-                Console.WriteLine("Server successfully started at" + baseAddress);
-                Console.WriteLine("Press key to shut down...");
-                Console.ReadLine();
-            }           
+                PrintStartupInfo(options);
+            }
+        }
+
+        private static void PrintStartupInfo(StartOptions options)
+        {
+            Console.WriteLine("Server successfully started at: ");
+            foreach (string url in options.Urls)
+            {
+                Console.WriteLine(url);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press key to shut down...");
+            Console.ReadLine();
+        }
+
+        private static StartOptions CreateStartupOptions()
+        {
+            const int port = 9000;
+            var options = new StartOptions();
+            options.Urls.Add(GetUrl("localhost", port));
+            options.Urls.Add(GetUrl("127.0.0.1", port));
+            options.Urls.Add(GetUrl(Environment.MachineName, port));
+            return options;
+        }
+
+        private static string GetUrl(string ip, int port)
+        {
+            return string.Format("http://{0}:{1}", ip, port);
         }
 
     }
