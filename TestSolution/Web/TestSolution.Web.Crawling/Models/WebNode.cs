e3b3660ea01f66578b97e8791a9f32ab87a6940c
pujubuju <truetormentedsoul@gmail.com>
2015-08-20 23:25:17 +0200

    crawler

---
+++ TestSolution/Web/TestSolution.Web.Crawling/Models/WebNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace TestSolution.Web.Crawling.Models
+{
+    public class WebNode
+    {
+
+        public List<WebNode> WebNodes { get; set; }
+        public WebNode Parent { get; set; }
+        public string Url { get; set; }
+
+        public WebNode(string url)
+        {
+            Url = url;
+        }
+
+        public WebNode(WebNode parentNode, string url)
+            :this(url)
+        {
+            Parent = parentNode;
+            WebNodes = new List<WebNode>();
+        }
+
+    }
+}
---
+++ TestSolution/Web/TestSolution.Web.Crawling/WebCrawler.cs
@@ -0,0 +1,20 @@
+﻿using TestSolution.Web.Crawling.Models;
+
+namespace TestSolution.Web.Crawling
+{
+    public class WebCrawler
+    {
+        public WebNode StartingNode { get; set; }
+
+        public WebCrawler()
+        {
+            
+        }
+
+        public void Crawl(string url, int level)
+        {
+            
+        }
+
+    }
+}
