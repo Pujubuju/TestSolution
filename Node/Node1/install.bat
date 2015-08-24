c7403fe9da2e0f2aa16ae4309acfec8e7fc6d0fe
pujubuju <truetormentedsoul@gmail.com>
2015-08-24 21:46:30 +0200

    node testing

---
+++ Node/Node1/install.bat
@@ -0,0 +1,13 @@
+echo install express node.js web app framework
+call npm install express --save;
+
+echo body-parser - This is a node.js middleware for handling 
+echo JSON, Raw, Text and URL encoded form data.
+call npm install body-parser --save;
+
+echo cookie-parser - Parse Cookie header and populate req.cookies 
+echo with an object keyed by the cookie names.
+call npm install cookie-parser --save;
+
+echo multer - This is a node.js middleware for handling multipart/form-data.
+call npm install multer --save;
