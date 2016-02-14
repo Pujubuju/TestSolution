c830f0fea9413d9285be18762751bdd808c30c17
pujubuju <truetormentedsoul@gmail.com>
2016-02-14 17:16:18 +0100

    tests running

--- NetCoreTutorial/NetCoreTutorial.Tests/Class1.cs
+++ NetCoreTutorial/NetCoreTutorial.Tests/Class1.cs
@@ -20,5 +20,19 @@ namespace NetCoreTutorial.Tests
         {
             return x + y;
         }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
     }
 }
